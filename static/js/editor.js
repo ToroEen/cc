f3dd790eaee8c3bd5f4a425d8b4582a6c59ed913
kipteam <toro.een@gmail.com>
2022-06-19 15:58:51 +0200

    Added line counter to bracket information

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,11 +1,6 @@
 function editor_start() {
   const input = document.getElementById('input');
-  const line_counter = document.getElementById('line-counter');
-
-  input.addEventListener('scroll', () => {
-    line_counter.scrollTop = input.scrollTop;
-    line_counter.scrollLeft = input.scrollLeft;
-  });
+  const line_counter = document.getElementsByClassName('line-counter');
   
   const information = document.getElementById('information');
 
@@ -13,10 +8,17 @@ function editor_start() {
   
   function updateValue(e) {
     let text = input.value.split("\n");
-    const total_lines = text.length;
+    
+    const filter_1 = document.querySelector('input[type="radio"]:checked').value.replace("sb", "[").replace("cb", "{").replace("rb", "(").replace("ab", "<");
+    const filter_2 = document.querySelector('input[type="radio"]:checked').value.replace("sb", "]").replace("cb", "}").replace("rb", ")").replace("ab", ">");
+
+    const total_lines = text.length + 1;
     let stop_counting = false;
 
-    line_counter.innerHTML = "";
+    for(var i = 0; i < line_counter.length; i++)
+    {
+      line_counter[i].innerHTML = "";
+    }
 
     while (information.firstChild) {
       information.removeChild(information.lastChild);
@@ -25,13 +27,26 @@ function editor_start() {
     text.forEach(function(entry) {
       let new_line = document.createElement("div");
 
-      new_line.innerHTML = "[: " + (entry.split('[').length - 1).toString() + " &emsp; " + "\]: " + (entry.split("]").length - 1).toString();
+      const value_1 = entry.split(filter_1).length - 1;
+      const value_2 = entry.split(filter_2).length - 1;
+
+      new_line.innerHTML = filter_1 + ": " + value_1.toString() + " &emsp; " + filter_2 + ": " + value_2.toString();
       
+      if (value_1 !== value_2) {
+        new_line.classList.add("warning")
+      } else if (new_line.classList.contains("warning")) {
+        new_line.classList.remove("warning")
+      }
+
       information.appendChild(new_line);
 
       if (!stop_counting) {
         for (var x = 1; x < total_lines; x++) {
-          line_counter.innerHTML += x.toString() + "<br>";
+          for(var i = 0; i < line_counter.length; i++)
+          {
+            line_counter[i].innerHTML += x.toString() + "<br>";
+          }
+      
           if (x === total_lines - 1) {
             stop_counting = true
           }
