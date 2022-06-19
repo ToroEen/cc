837efc9863a6581bfa009221aa93a3e261c518d4
kipteam <toro.een@gmail.com>
2022-06-19 10:56:09 +0200

    Added line counter to editor

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,24 +1,42 @@
 function editor_start() {
   const input = document.getElementById('input');
+  const line_counter = document.getElementById('line-counter');
+
+  input.addEventListener('scroll', () => {
+    line_counter.scrollTop = input.scrollTop;
+    line_counter.scrollLeft = input.scrollLeft;
+  });
+  
   const information = document.getElementById('information');
 
   input.addEventListener('input', updateValue);
   
   function updateValue(e) {
-    const square_left = document.getElementById("square_left");
-    
-    let text = input.value.split("\n")
+    let text = input.value.split("\n");
+    const total_lines = text.length;
+    let stop_counting = false;
+
+    line_counter.innerHTML = "";
 
     while (information.firstChild) {
       information.removeChild(information.lastChild);
     }
 
     text.forEach(function(entry) {
-      let myElm = document.createElement("div");
+      let new_line = document.createElement("div");
 
-      myElm.innerHTML = "[: " + (entry.split('[').length - 1).toString() + " &emsp; " + "\]: " + (entry.split("]").length - 1).toString();
+      new_line.innerHTML = "[: " + (entry.split('[').length - 1).toString() + " &emsp; " + "\]: " + (entry.split("]").length - 1).toString();
       
-      information.appendChild(myElm);
+      information.appendChild(new_line);
+
+      if (!stop_counting) {
+        for (var x = 1; x < total_lines; x++) {
+          line_counter.innerHTML += x.toString() + "<br>";
+          if (x === total_lines - 1) {
+            stop_counting = true
+          }
+        } 
+      }
     });
   }
 }
