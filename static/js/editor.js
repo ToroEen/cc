32727b31b2230c21a541a13913748664b38800cb
kipteam <toro.een@gmail.com>
2022-06-19 10:09:02 +0200

    Basic system in place

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,13 +1,24 @@
 function editor_start() {
   const input = document.getElementById('input');
+  const information = document.getElementById('information');
 
   input.addEventListener('input', updateValue);
   
   function updateValue(e) {
     const square_left = document.getElementById("square_left");
     
-    text = input.value
-    
-    square_left.innerText = text.split("[").length - 1;
+    let text = input.value.split("\n")
+
+    while (information.firstChild) {
+      information.removeChild(information.lastChild);
+    }
+
+    text.forEach(function(entry) {
+      let myElm = document.createElement("div");
+
+      myElm.innerHTML = "[: " + (entry.split('[').length - 1).toString() + " &emsp; " + "\]: " + (entry.split("]").length - 1).toString();
+      
+      information.appendChild(myElm);
+    });
   }
 }
