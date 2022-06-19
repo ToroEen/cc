f808ff6253319189519bd5cd22db09f2692a1dc1
kipteam <toro.een@gmail.com>
2022-06-19 17:30:16 +0200

    Polished the bottom settings bar

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Bracket counters are done
-Website that can count all types of brackets -> [, ], (, ), {, }
+Website that can count all types of brackets -> [, ], (, ), {, } <br>
 Also other things ig -> <, >
--- static/js/editor.js
+++ static/js/editor.js
@@ -3,16 +3,18 @@ function editor_start() {
   const line_counter = document.getElementsByClassName('line-counter');
   
   const information = document.getElementById('information');
+  const total = document.getElementById("total");
 
   input.addEventListener('input', updateValue);
   
-  function updateValue(e) {
+  function updateValue() {
     let text = input.value.split("\n");
     
-    const filter_1 = document.querySelector('input[type="radio"]:checked').value.replace("sb", "[").replace("cb", "{").replace("rb", "(").replace("ab", "<");
-    const filter_2 = document.querySelector('input[type="radio"]:checked').value.replace("sb", "]").replace("cb", "}").replace("rb", ")").replace("ab", ">");
+    const filter_1 = document.getElementById("filter_input").value.replace("sb", "[").replace("cb", "{").replace("rb", "(").replace("ab", "<");
+    const filter_2 = document.getElementById("filter_input").value.replace("sb", "]").replace("cb", "}").replace("rb", ")").replace("ab", ">");
 
     const total_lines = text.length + 1;
+
     let stop_counting = false;
 
     for(var i = 0; i < line_counter.length; i++)
@@ -29,6 +31,8 @@ function editor_start() {
 
       const value_1 = entry.split(filter_1).length - 1;
       const value_2 = entry.split(filter_2).length - 1;
+
+      total.innerHTML = filter_1 + ": " + (input.value.split(filter_1).length - 1).toString() + "&emsp;" + filter_2 + ": " + (input.value.split(filter_2).length - 1).toString();
 
       new_line.innerHTML = filter_1 + ": " + value_1.toString() + " &emsp; " + filter_2 + ": " + value_2.toString();
       
@@ -54,4 +58,18 @@ function editor_start() {
       }
     });
   }
+}
+
+function change_filter(new_value) {
+  const ids = ["sb", "cb", "rb", "ab"];
+
+  document.getElementById("filter_input").value = new_value;
+
+  ids.forEach(function(id) {
+    if (document.getElementById(id).classList.contains("clicked")) {
+      document.getElementById(id).classList.remove("clicked")
+    }
+  });
+
+  document.getElementById(new_value).classList.add("clicked");
 }
